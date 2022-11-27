02d9f0efd9440ee740ae6b14b001ad3073b3a05c
Sanjeev85 <sanjeevchoubey215@gmail.com>
2022-11-27 17:52:32 +0530

    rendring props through function

--- src/App.js
+++ src/App.js
@@ -1,49 +1,38 @@
-import './App.css';
-import { useRef } from "react"
+import React from "react";
+import "./App.css";
 
+const tahoe_peaks = [
+  { name: "Freel", elevation: 10891 },
+  { name: "Monument", elevation: 10067 },
+  { name: "Pyramid", elevation: 9983 },
+  { name: "Tallac", elevation: 9735 },
+];
 
-
-function App() {
-  const txtTitle = useRef()
-  const hexColor = useRef()
-
-
-  const submit = (e) => {
-    e.preventDefault()
-    const title = txtTitle.current.value
-    const color = hexColor.current.value
-    alert(`${title} ${color}`)
-    txtTitle.current.value = ""
-    hexColor.current.value = ""
-  };
-
-
-  return (
-    <form onSubmit={submit}>
-      <input ref={txtTitle} type="text" placeholder='color title..' />
-      <input ref={hexColor} type="color" />
-      <button>Add</button>
-    </form>
-  )
+function List({ data, renderItem, renderEmpty }) {
+  console.log(data);
+  return !data.length ? (
+    renderEmpty
+  ) : (
+    <ul>
+      {data.map((item) => {
+        return <li key={item.name}>{renderItem(item)}</li>;
+      })}
+    </ul>
+  );
 }
 
+function App() {
+  return (
+    <List
+      data={tahoe_peaks}
+      renderEmpty={<p>List is Empty</p>}
+      renderItem={(item) => (
+        <>
+          {item.name} -- {item.elevation} ft.
+        </>
+      )}
+    />
+  );
+}
 
-// function App({library}) {
-//   const [emotion, setEmotion] = useState("happy")
-//   useEffect(()=>{
-//     console.log(`It's ${emotion} right now`)
-//   }, [emotion])
-
-//   let toogleEmotion = () => {
-//     if (emotion === 'happy') return 'sad'
-//     else return 'happy'
-//   }
-
-//   return (
-//     <div className="App">
-//       <h1>Current emotion is {emotion}</h1>
-//       <button onClick={()=> setEmotion(toogleEmotion())}>toogle Emotion</button>
-//     </div>
-//   );
-// }
 export default App;
--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ import AsyncReact from "./AsyncReact";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AsyncReact />
+    <App />
   </React.StrictMode>
 );
